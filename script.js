728b8c781056dc1339de29e3984043403376e4bb
MOHAN_VIKRAM <mohanvikram668@gmail.com>
2023-09-21 23:06:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
-//your code here
+let touristSpots = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
+let articleless = [] ;
 
+for(let i=0; i<touristSpots-1; i++){
+	let articlelessitem = arr[i].replace(regexp, "").trim();
+	articleless.push(articlelessitem);
+}
+console.log(articleless);
