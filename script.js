966591fc022287a3ccae2a9cf525e717fb7842af
MOHAN_VIKRAM <mohanvikram668@gmail.com>
2023-09-23 12:52:04 +0530

    created a submission

--- script.js
+++
@@ -1,8 +0,0 @@
-let touristSpots = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
-let articleless = [] ;
-
-for(let i=0; i<touristSpots-1; i++){
-	let articlelessitem = arr[i].replace(regexp, "").trim();
-	articleless.push(articlelessitem);
-}
-console.log(articleless);
